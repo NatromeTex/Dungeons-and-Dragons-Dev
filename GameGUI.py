3eeb725d496725aee8929fedfcd88750ccb7516f
NatromeTex <natrometex2014@gmail.com>
2023-12-30 03:01:55 +0530

    Added: Weapon ids to every item Added: Font for the game

--- GameGUI.py
+++ GameGUI.py
@@ -2,13 +2,14 @@ import pygame
 
 class GameGUI():
     def __init__(self):
+        pygame.init()
         self.running = True
         self.playing = False
         self.START_KEY, self.DOWN_KEY, self.UP_KEY, self.BACK_KEY = False, False, False,False
         self.DISPLAY_W, self.DISPLAY_H = 1280, 720
         self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
         self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
-        self.font_name = "Assets/BreathFire.ttf"
+        self.font_name = "BreatheFire.ttf"
         self.BLACK, self.WHITE = (0,0,0),(255,255,255)
 
     def game_loop(self):
@@ -17,6 +18,7 @@ class GameGUI():
             if self.START_KEY:
                 self.playing = False
             self.display.fill(self.BLACK)
+            self.drawText('Thanks for playing',80,self.DISPLAY_W/2,self.DISPLAY_H/2)
             self.window.blit(self.display, (0,0))
             pygame.display.update()
             self.resetKeys
@@ -34,6 +36,14 @@ class GameGUI():
                     self.UP_KEY = True
                 if event.key == pygame.K_DOWN:
                     self.DOWN_KEY = True
+    
+    def drawText(self, text, size, x, y):
+        font = pygame.font.Font(self.font_name, size)
+        textSurface = font.render(text, True, self.WHITE)
+        textRect = textSurface.get_rect()
+        textRect.center = (x,y)
+        self.display.blit(textSurface,textRect)
+
     def resetKeys(self):
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
             
